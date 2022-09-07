c0fe1b58412f77f9e96decc4edf01ee0c462817a
bengal <bengal1@mail.tau.ac.il>
2022-09-07 18:52:12 +0300

    Added some explanations

--- differentialEq/FirstOrderDEO.py
+++ differentialEq/FirstOrderDEO.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 ran = [0, 5]
-N = 30
+N = 25
 h = (ran[1] - ran[0])/N
 y_0, t_0 = 1, 0
 a = [0, -1, 0]    
@@ -12,7 +12,7 @@ def analytic_sol(t):
     return np.exp(a[1]*t)
 
 def dervative(t, y, a, b):
-    return (a[0] + a[1]*y + a[2]*(y**2))*(b[0] + b[1]*t + b[2]*(t**2))
+    return (a[0] + a[1]*y)
 
 def t(n):
     return t_0 + h*n 
@@ -25,9 +25,10 @@ def y(n):
  
 t_n = np.array([t(n) for n in range(N)])
 y_n = np.array([y(n) for n in range(N)])
-plt.scatter(t_n, y_n)
+plt.scatter(t_n, y_n, label='First Order')
 
 y_t = np.array(analytic_sol(t_n))
-plt.plot(t_n, y_t)
+plt.plot(t_n, y_t, label='exp(-x)')
 
+plt.legend()
 plt.show()
